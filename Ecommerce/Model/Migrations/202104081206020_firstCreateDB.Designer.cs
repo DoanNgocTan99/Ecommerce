6809a1165f64e4a46a5c560029b9678a27ff5c27
nguyenbae1712 <34834564+nguyenbae1712@users.noreply.github.com>
2021-04-08 19:35:11 +0700

    migration

--- Ecommerce/Model/Migrations/202104081206020_firstCreateDB.Designer.cs
+++ Ecommerce/Model/Migrations/202104081206020_firstCreateDB.Designer.cs
@@ -7,13 +7,13 @@ namespace Model.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.4.4")]
-    public sealed partial class init : IMigrationMetadata
+    public sealed partial class firstCreateDB : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(firstCreateDB));
         
         string IMigrationMetadata.Id
         {
-            get { return "202104080210355_init"; }
+            get { return "202104081206020_firstCreateDB"; }
         }
         
         string IMigrationMetadata.Source
