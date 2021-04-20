80660ba3ebc79081187ea130e4eefe3d1ded21cb
Tan Doan <tandoan@sapotacorp.com>
2021-04-20 10:08:22 +0700

    commit adminController

---
+++ Ecommerce/Ecommerce/Areas/Seller/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecommerce.Areas.Seller.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Seller/Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
